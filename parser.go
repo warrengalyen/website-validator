b621087128155186b8a3bed6ea2d52173812182e
warrengalyen <wgalyen@hotmail.com>
2024-05-01 15:20:10 -0700

    Don't break with invalid URL match in CSS

--- parser.go
+++ parser.go
@@ -371,7 +371,7 @@ func fetchAndParse(httplink, action string, depth int, wg *sync.WaitGroup) {
 		for _, link := range extractStyleURLs(string(body)) {
 			full, err := absoluteURL(link, httplink)
 			if err != nil {
-				return
+				continue
 			}
 			if isMixedContent(httplink, full) {
 				errorsProcessed++
